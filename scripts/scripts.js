80835c974d62242a9b54f69a8bd3df23357a3598
zhangzifeng <>
2022-05-06 13:29:16 -0400

    about page editing

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,21 @@
 /* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
+
+
+/*Navigation Bar*/
+$(document).ready(function() {
+    $(".menu-icon").on("click", function() {
+          $("nav ul").toggleClass("showing");
+    });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+          $('nav').addClass('black');
+    }
+
+    else {
+          $('nav').removeClass('black');
+    }
+})
